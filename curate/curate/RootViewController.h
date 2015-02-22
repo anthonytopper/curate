d1f5fa8e720d2c220c66bd034364cc516f7f4ffd
aniiyengar <aniruddh.iyengar202@gmail.com>
2015-02-22 11:59:45 -0500

    more ui shit

---
+++ curate/curate/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  curate
+//
+//  Created by Xcode Projects on 2/22/15.
+//  Copyright (c) 2015 Curaté. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
