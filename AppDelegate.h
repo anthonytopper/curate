5fac802c26284b8db493c3dbdcd4ef458edf8fa3
Abhinav Kurada <abhinav.kurada@gmail.com>
2016-06-17 17:51:57 -0400

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Curaté
+//
+//  Created by Abhinav Kurada on 2/20/15.
+//  Copyright (c) 2015 Abhinav Kurada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Curaté
+//
+//  Created by Abhinav Kurada on 2/20/15.
+//  Copyright (c) 2015 Abhinav Kurada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
